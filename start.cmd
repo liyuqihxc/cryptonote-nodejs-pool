ac9ab6f370e8b6a862cf2a954164b83d846b09f6
liyuqihxc <liyuqihxc@gmail.com>
2018-11-05 23:05:55 +0800

    private testnet

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-docker run --rm -it -p 8117:8117/tcp -p 3333:3333/tcp -p 5555:5555/tcp -p 7777:7777/tcp -v "/d/documents/visual studio 2017/projects/cryptonote-nodejs-pool/config_examples:/config" cryptonote-nodejs-pool:latest /bin/bash
+docker run --rm -it -p 8117:8117/tcp -p 3333:3333/tcp -p 5555:5555/tcp -p 7777:7777/tcp -v "/e/documents/visual studio 2015/projects/cryptonote-nodejs-pool/config_examples:/config" cryptonote-nodejs-pool:latest /bin/bash
